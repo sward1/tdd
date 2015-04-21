7785955fa30550b7c6b88938c3e787645b1f9fde
sward1 <stephenjward1@gmail.com>
2015-04-21 14:52:34 -0400

    Custom Persona auth backend + custom user model

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def persona_login(request):
+    user = authenticate(assertion=request.POST['assertion'])
+    if user:
+        login(request, user)
+    return HttpResponse('OK')
